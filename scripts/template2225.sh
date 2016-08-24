65e410ff565a774da4d8df7714b409aa5c33b4b8
ignrodri <ignrodri@ucm.es>
2016-08-24 17:10:53 +0200

    commit

--- scripts/template2225.sh
+++ scripts/template2225.sh
@@ -4,5 +4,6 @@ export PATH=/home/ec2-user/bin/antsbin/bin:$PATH
 export ANTSPATH=/home/ec2-user/bin/antsbin/bin
 export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=1
 
+cd /home/ec2-user/xtfm/images
 buildtemplateparallel.sh -d 3 -o prev2225 -c 2 -j 4 -m 1x0x0 2225*T1masked.nii.gz
 buildtemplateparallel.sh -d 3 -o post2225 -c 2 -j 4 -z prev2225template.nii.gz 2225*T1masked.nii.gz
