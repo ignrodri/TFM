bed974c527e08138713781de3d01867972557bf2
ignrodri <ignrodri@ucm.es>
2016-08-24 18:17:53 +0200

    commit

---
+++ scripts/template2459.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export PATH=/home/ec2-user/bin/antsbin/bin:$PATH
+export ANTSPATH=/home/ec2-user/bin/antsbin/bin
+export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=1
+
+cd /home/ec2-user/xtfm/images
+buildtemplateparallel.sh -d 3 -o prev2459 -c 2 -j 16 -m 1x0x0 2459*T1masked.nii.gz
+buildtemplateparallel.sh -d 3 -o post2459 -c 2 -j 16 -z prev2459template.nii.gz 2459*T1masked.nii.gz
---
+++ scripts/template2502.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export PATH=/home/ec2-user/bin/antsbin/bin:$PATH
+export ANTSPATH=/home/ec2-user/bin/antsbin/bin
+export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=1
+
+cd /home/ec2-user/xtfm/images
+buildtemplateparallel.sh -d 3 -o prev2502 -c 2 -j 16 -m 1x0x0 2502*T1masked.nii.gz
+buildtemplateparallel.sh -d 3 -o post2502 -c 2 -j 16 -z prev2502template.nii.gz 2502*T1masked.nii.gz
