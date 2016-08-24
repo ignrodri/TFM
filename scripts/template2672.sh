520051bfa1877425cc007d46d6091af6ce93ea46
ignrodri <ignrodri@ucm.es>
2016-08-24 18:52:58 +0200

    commit

---
+++ scripts/template2672.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export PATH=/home/ec2-user/bin/antsbin/bin:$PATH
+export ANTSPATH=/home/ec2-user/bin/antsbin/bin
+export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=1
+
+cd /home/ec2-user/xtfm/images
+buildtemplateparallel.sh -d 3 -o prev2672 -c 2 -j 16 -m 1x0x0 2672*T1masked.nii.gz
+buildtemplateparallel.sh -d 3 -o post2672 -c 2 -j 16 -z prev2672template.nii.gz 2672*T1masked.nii.gz
