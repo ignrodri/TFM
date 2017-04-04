645447f728cb1d9e4ee1a7ea12c344e6f927818d
ignrodri <ignrodri@ucm.es>
2017-04-04 12:38:12 +0200

    commit

--- scripts/heards2017.sh
+++ scripts/heards2017.sh
@@ -5,5 +5,5 @@ export ANTSPATH=/home/ec2-user/bin/antsbin/bin
 export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=1
 
 cd /home/ec2-user/xtfm/images
-heards2017.py
+./heards2017.py
 cp *.nii.gz /home/ec2-user/TFM/images
