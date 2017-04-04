9df392d01e6afb52664c85849916534961f8740f
ignrodri <ignrodri@ucm.es>
2017-04-04 14:03:23 +0200

    commit

--- scripts/heards2017.sh
+++ scripts/heards2017.sh
@@ -6,4 +6,5 @@ export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=1
 
 cd /home/ec2-user/xtfm/images
 /home/ec2-user/xtfm/scripts/heards2017.py
+/bin/rm -f /home/ec2-user/xtfm/images/stop
 cp *.nii.gz /home/ec2-user/TFM/images
