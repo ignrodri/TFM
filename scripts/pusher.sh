d0681809e0275ca3174c655c3edd5f2a3cce009a
ignrodri <ignrodri@ucm.es>
2017-04-04 15:08:07 +0200

    commit

--- scripts/pusher.sh
+++ scripts/pusher.sh
@@ -3,6 +3,7 @@
 mv -f /home/ec2-user/TFM/images/*.mat /home/ec2-user/xtfm/images
 mv -f /home/ec2-user/TFM/images/*Warp.nii.gz /home/ec2-user/xtfm/images
 mv -f /home/ec2-user/TFM/images/*Warped.nii.gz /home/ec2-user/xtfm/images
+/bin/rm -f /home/ec2-user/TFM/images/stop
 cd /home/ec2-user/TFM
 git add --all .
 git commit -m "commit"
