c5c667f0420f265d4cd159d9ec77d980e58551bd
ignrodri <ignrodri@ucm.es>
2017-04-17 02:10:45 +0200

    commit

--- scripts/heards2017.py
+++ scripts/heards2017.py
@@ -19,13 +19,19 @@ for s1 in str1:
 		ft1right = fst + '-T1hpright.nii.gz'
 		fdtileft = fst + '-DTIhpleft.nii.gz'
 		fdtiright = fst + '-DTIhpright.nii.gz'
+		freg = 'antsRegistrationSyNQuick.sh -d 3 -f ' + fmask + ' -m ' + fdti + ' -o ' + fst + '-DTI-'
+		fleft = 'antsApplyTransforms -d 3 -i ' + ft1left + ' -o ' + fdtileft + ' -r ' + fdti + '-t [' + fmat + ',1] -t ' + fwarp
+		fright = 'antsApplyTransforms -d 3 -i ' + ft1right + ' -o ' + fdtiright + ' -r ' + fdti + '-t [' + fmat + ',1] -t ' + fwarp
 		
 		if not os.path.isfile(fstop) and not os.path.isfile(fmat) and os.path.isfile(fmask) and os.path.isfile(fdti):
 			os.system('touch ' + fst + 'mat.x')
-			os.system('antsRegistrationSyNQuick.sh -d 3 -f ' + fmask + ' -m ' + fdti + ' -o ' + fst + '-DTI-')
+			os.system('echo \"' + freg + '\"')
+			os.system(freg)
 		if not os.path.isfile(fstop) and not os.path.isfile(fdtileft) and os.path.isfile(ft1left) and os.path.isfile(fmat):
 			os.system('touch ' + fst + 'left.x')
-			os.system('antsApplyTransforms -d 3 -i ' + ft1left + ' -o ' + fdtileft + ' -r ' + fdti + '-t [' + fmat + ',1] -t ' + fwarp)
+			os.system('echo \"' + fleft + '\"')
+			os.system(fleft)
 		if not os.path.isfile(fstop) and not os.path.isfile(fdtiright) and os.path.isfile(ft1right) and os.path.isfile(fmat):
 			os.system('touch ' + fst + 'right.x')
-			os.system('antsApplyTransforms -d 3 -i ' + ft1right + ' -o ' + fdtiright + ' -r ' + fdti + '-t [' + fmat + ',1] -t ' + fwarp)
+			os.system('echo \"' + fright + '\"')
+			os.system(fright)
